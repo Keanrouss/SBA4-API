71241a95c39393586d514ada1f287c9688846d97
keanrouss <timajori1@gmail.com>
2025-06-06 18:10:21 -0400

    7th commit

--- SBA4.js
+++ SBA4.js
@@ -1,60 +1,22 @@
-//has the API information for this SBA
-//async function getdata() {
-    //const response = await fetch((' http://discord.jikan.moe.'))
-
-    //try {
-        //if (response.ok) {
-           // let results = await response.json()
-            //console.log(results)
-        //} else {
-
-       // }
-
-   // } catch (error) {
-
-   // }
-//}
-
-//getdata()
 async function getdata() {
-//const url = ('https://dattebayo-api.onrender.com');
-//const options = {
-	//method: 'GET', //none of the API were working for me. got from RapiAPI and it came with GET. Not 100% familiar
-//};
+    try {
+        const response = await fetch('https://dattebayo-api.onrender.com/characters');
+        const data = await response.json()
+        const characters = data.characters
 
-try {
-    const response = await fetch('https://dattebayo-api.onrender.com/characters');
-	const data= await response.json()
-    const characters = data.characters
-    //console.log(characters)
-    //display the names and the intented data to create the profile card for characters// 
-    // the for(let character of characters) don't know how to navigate that. More comfortable with let i etc..loop
-    for(let character of characters){
-        //console.log(characters)
-
-    //}
-        //for (let i =0; i<characters.length; i++) {
-            //console.log(data.characters[i])
-            //console.log(data.characters[i].personal.birthdate) //personal provide the info (dob, sex, clan,classification)
-            //console.log(data.characters[i].personal.clan)
-            //console.log(data.characters[i].images)
-            //some characters have no clan, therefore undefined is not wrong.
-            //just no clan informaton for these characters. Will write Not available.
-            }
-            //to narrow down what the users need to input to get data about characters.
-            //declare a new variable to run the condition..Leave it empty?.....
-            const userinput = ""
-            //Making sure input matched exactly how char. name is written
-            const selectedcharacter = characters.some(char => char.name.toLowerCase()=== userinput.toLowerCase)
-             if (userinput) {
+        for (let character of characters) {
+        }
+        const userinput = ""
+        const selectedcharacter = characters.some(char => char.name.toLowerCase() === userinput.toLowerCase)
+        if (userinput) {
             console.log("Proper character selected")
-        } else{
-           console.log("Put Naruto anime main characters only")
-     }
-   
-}catch(error) {
-	console.error(error);
-}
+        } else {
+            console.log("Put Naruto anime main characters only")
+        }
+
+    } catch (error) {
+        console.error(error);
+    }
 }
 let button = document.querySelector(".click")
 button.addEventListener("click", getdata)
--- SBA4body.js
+++ SBA4body.js
@@ -1,5 +1,8 @@
 //all of the buttons/information for the html created in the dom
 let body = document.querySelector("body")
+let select = document.createElement('select');
+
+body.appendChild(select);
 //Title/banner information
 let Narutotitle = document.querySelector('h2')
 Narutotitle.style.display = "flex";
@@ -18,12 +21,18 @@ image.src= ""
 async function getcharacterprofile() {
     const response = await fetch('https://dattebayo-api.onrender.com/characters');
 	const data= await response.json()
-    const characters = data.characters[0].name
+    const characters = data.characters
     console.log(characters)
-    image.src = data.characters[5].images[1]
+    
+    characters.forEach((value, key)=>{
+             let options = document.createElement('option');
+        select.appendChild(options);
+        options.innerText = value.name
+            console.log(value.name)
+        })
 
-let mainContainer = document.createElement('div')
-let namebanner= document.createElement('h2')
+ mainContainer = document.createElement('div')
+ namebanner= document.createElement('h2')
 namebanner.textContent= characters.name 
 mainContainer.appendChild(namebanner)
 
