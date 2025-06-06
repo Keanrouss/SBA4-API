25f62446a82709f6dd3af964a93baa85fc674033
keanrouss <timajori1@gmail.com>
2025-06-06 09:50:41 -0400

    5th commit

---
+++ SBA4.2.js
@@ -0,0 +1,1 @@
+const charactercontainer = document.createElement('characters.name')
--- SBA4.js
+++ SBA4.js
@@ -22,31 +22,32 @@ async function getdata() {
 	//method: 'GET', //none of the API were working for me. got from RapiAPI and it came with GET. Not 100% familiar
 //};
 
-//try {
+try {
     const response = await fetch('https://dattebayo-api.onrender.com/characters');
 	const data= await response.json()
     const characters = data.characters
-
     console.log(characters)
-         for (let character of characters) {
-            
-            //console.log(characters.characters[i].name)
-            //console.log(characters.characters[i].name)
-            // data.[2].name
-             }
-         // data.data[2].title
+    //display the names and the intented data to create the profile card for characters// 
+    // the for(let character of characters) don't know how to navigate that. More comfortable with let i etc..loop
+         for (let i =0; i<characters.length; i++) {
+            console.log(data.characters[i])
+            console.log(data.characters[i].personal) //personal provide the info (dob, sex, clan,classification)
+            }
+            //to narrow down what the users need to input to get data about characters.
+            //declare a new variable to run the condition..Leave it empty?.....
+            const userinput = ""
+            //Making sure input matched exactly how char. name is written
+            const selectedcharacter = characters.some(char => char.name.toLowerCase()=== userinput.toLowerCase)
+             if (userinput) {
+            console.log("Proper character selected")
+        } else{
+           console.log("Put Naruto main characters only")
+     }
    
-             // if (ranking > 9000) {
-            //console.log('Favorite episode to watch later!')
-      //  } else if(ranking < 7000) {
-           // console.log("Just a terrible video!")
-      //  }
-   // }
-//} catch (error) {
-	//console.error(error);
-//}
-	}
-
+}catch(error) {
+	console.error(error);
+}
+}
 let button = document.querySelector(".click")
 button.addEventListener("click", getdata)
 //getdata()
@@ -66,5 +67,7 @@ button.addEventListener("click", getdata)
 	//console.log(data.data[0].ranking);
    // let ranking = data.data[0].ranking
     //console.log(ranking)
+    // console.log(data.characters[0].name)
+            // data.[2].name
 
 
